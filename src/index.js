a28b3cf5e76018c9555dafd4e39a4124d98148c4
Evgeniya K <evgexa44@gmail.com>
2020-10-01 17:25:06 +0300

    init: start Typical Arrays Problems task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+    let minValue = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] < minValue) {
+            minValue = array[i];
+        }
+    }
+    console.log(minValue);
+    return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+    let maxValue = array[0];
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > maxValue) {
+            maxValue = array[i];
+        }
+    }
+    console.log(maxValue);
+    return 0;
 }
 
 exports.avg = function avg (array) {
+    let avgValue = array[0];
+    for (let i = 1; i < array.length; i++) {
+        avgValue += array[i];
+    }
+    console.log(avgValue / array.length);
   return 0;
 }
+
+
